7de82142224d8571298db104a2035a879c60c6c1
maisawa <k023g1023@g.neec.ac.jp>
2024-06-25 10:53:11 +0900

    02_05

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,4 +1,3 @@
-
 #include "GameScene.h"
 #include "TextureManager.h"
 #include "myMath.h"
@@ -11,7 +10,6 @@ GameScene::~GameScene() {
 	delete modelBlock_;
 	delete modelSkydome_;
 	delete mapChipField_;
-	// 自キャラの開放
 	delete player_;
 
 	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
@@ -30,55 +28,32 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-
-	// ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
-	// 3Dモデルの生成
 	model_ = Model::Create();
 	modelBlock_ = Model::Create();
 	modelSkydome_ = Model::CreateFromOBJ("sphere", true);
-
-	// ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
-	// ビュープロジェクションの初期化
 	viewProjection_.farZ;
 	viewProjection_.Initialize();
-
-	// 天球の生成
 	skydome_ = new Skydome();
-	// 天球の初期化
 	skydome_->Initialize(modelSkydome_, &viewProjection_);
-
-	// マップチップの描画の生成
 	mapChipField_ = new MapChipField;
 	mapChipField_->LoadMapChipCsv("Resources/map.csv");
-	// マップチップの描画の初期化
 	mapChipField_->Initialize(model_, &viewProjection_);
 	GenerateBlocks();
-
-	// 自キャラの生成
 	player_ = new Player();
-
 	Vector3 playerPosition = mapChipField_->MapChipPositionByIndex(30, 0);
-
-	// 自キャラの初期化
 	player_->Initialize(model_, &viewProjection_, playerPosition);
-
-	// デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
 }
 
 void GameScene::GenerateBlocks() {
-	// 要素数
+
 	const uint32_t numBlockVirtical = mapChipField_->GetkNumkBlockVirtical();
 	const uint32_t numBlockHorizontal = mapChipField_->GetkNumkBlockHorizontal();
-	// ブロック1個分の横幅
 	const float kBlockWidth = 2.0f;
 	const float kBlockHeight = 2.0f;
-	// 要素数を変更する
 	worldTransformBlocks_.resize(numBlockVirtical);
-
-	// キューブの生成
 	for (uint32_t i = 0; i < numBlockVirtical; ++i) {
 		worldTransformBlocks_[i].resize(numBlockHorizontal);
 	}
@@ -106,35 +81,25 @@ void GameScene::Update() {
 			isDebugCameraActive_ = true;
 	}
 #endif
-
-	// カメラ処理
 	if (isDebugCameraActive_) {
-		// デバッグカメラの更新
 		debugCamera_->Update();
 		viewProjection_.matView = debugCamera_->GetViewProjection().matView;
 		viewProjection_.matProjection = debugCamera_->GetViewProjection().matProjection;
-		// ビュープロジェクション行列の転送
 		viewProjection_.TransferMatrix();
 	} else {
-		// ビュープロジェクション行列の更新と転送
 		viewProjection_.UpdateMatrix();
 	}
-
-	// 自キャラの更新
 	player_->Update();
 
-	// 縦横ブロック更新
 	for (std::vector<WorldTransform*> worldTransformBlockTate : worldTransformBlocks_) {
 		for (WorldTransform* worldTransformBlockYoko : worldTransformBlockTate) {
 			if (!worldTransformBlockYoko)
 				continue;
 
-			// アフィン変換行列の作成
 			worldTransformBlockYoko->UpdateMatrix();
 		}
 	}
 
-	// 天球の更新
 	skydome_->Update();
 
 	mapChipField_->Update();
@@ -173,7 +138,6 @@ void GameScene::Draw() {
 
 	mapChipField_->Draw();
 
-	// 縦横ブロック描画
 	for (std::vector<WorldTransform*> worldTransformBlockTate : worldTransformBlocks_) {
 		for (WorldTransform* worldTransformBlockYoko : worldTransformBlockTate) {
 			if (!worldTransformBlockYoko)
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Audio.h"
 #include "DebugCamera.h"
 #include "DirectXCommon.h"
@@ -9,11 +8,8 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-
 #include <vector>
-
 #include "Skydome.h"
-
 #include "MapChipField.h"
 
 /// <summary>
@@ -57,31 +53,16 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
-	// テクスチャハンドル
 	uint32_t textureHandle_ = 0;
-	// 3Dモデル
 	Model* model_ = nullptr;
 	Model* modelBlock_ = nullptr;
 	Model* modelSkydome_ = nullptr;
-	// ワールドトランスフォームの初期化
 	WorldTransform worldTransform_;
-	// ビュープロジェクションの初期化
 	ViewProjection viewProjection_;
-
-	// 自キャラ
 	Player* player_ = nullptr;
-
-	// 縦横ブロック配列
 	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
-
-	// デバッグカメラ有効
 	bool isDebugCameraActive_ = false;
-	// デバッグカメラ
 	DebugCamera* debugCamera_ = nullptr;
-
-	// 天球
 	Skydome* skydome_ = nullptr;
-
-	//マップチップの描画
 	MapChipField* mapChipField_ = nullptr;
 };
