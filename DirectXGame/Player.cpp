afe8300c786f11195ed5892f1cbfdb6a71579fc8
maisawa <k023g1023@g.neec.ac.jp>
2024-06-25 11:09:49 +0900

    02_05

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,11 +1,13 @@
+#define NOMINMAX
 #include "Player.h"
 #include "myMath.h"
 #include <Input.h>
 #include <algorithm>
 #include <cassert>
 #include <numbers>
+
 void Player::Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position) {
-	// NULLポインタチェック
+
 	assert(model);
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
@@ -13,47 +15,130 @@ void Player::Initialize(Model* model, ViewProjection* viewProjection, const Vect
 	model_ = model;
 	viewProjection_ = viewProjection;
 }
-void Player::Update(){
-if(onGround_){
-  if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
-  Vector3 acceleration = {};
-    if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
-       if (velocity_.x < 0.0f) {
 
-           velocity_.x *= (1.0f - kAttenuation);
-       }
-	   acceleration.x += kAcceleratio;
-	   if (lrDirection_ != LRDirection::kRight) {
-           lrDirection_ = LRDirection::kRight;
+void Player::Update() {
 
-           turnFirstRotationY_ = worldTransform_.rotation_.y;
+	if (onGround_) {
+		if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
 
-           turnTimer_ = kTimeTurn;
-       }
-    } 
-	else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
-         if (velocity_.x > 0.0f) {
+			Vector3 acceleration = {};
 
-             velocity_.x *= (1.0f - kAttenuation);
-         }
+			if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
 
-         acceleration.x -= kAcceleratio;
+				if (velocity_.x < 0.0f) {
 
-         if (lrDirection_ != LRDirection::kLeft) {
-             lrDirection_ = LRDirection::kLeft;
-             turnFirstRotationY_ = worldTransform_.rotation_.y;
-             turnTimer_ = kTimeTurn;
-         }
-    }
-    operator+=(velocity_, acceleration);
+					velocity_.x *= (1.0f - kAttenuation);
+				}
 
-    velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+				acceleration.x += kAcceleratio;
 
-  }
+				if (lrDirection_ != LRDirection::kRight) {
+					lrDirection_ = LRDirection::kRight;
+
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+			} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+				if (velocity_.x > 0.0f) {
+					
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+
+				acceleration.x -= kAcceleratio;
+
+				if (lrDirection_ != LRDirection::kLeft) {
+					lrDirection_ = LRDirection::kLeft;
+				
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+			}
+	
+			operator+=(velocity_, acceleration);
+
+			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+
+			if (acceleration.x >= 0.01f || acceleration.x <= -0.01f) {
+				acceleration.x = 0;
+			}
+
+			// 旋回制御
+			if (turnTimer_ > 0.0f) {
+				// タイマーのカウントダウン
+				turnTimer_ = (1.0f / 60.0f);
+
+				// 左右の自キャラ角度テーブル
+				float destinationRotationYTable[] = {
+				    std::numbers::pi_v<float> / 2.0f,
+				    std::numbers::pi_v<float> * 3.0f / 2.0f,
+				};
+				// 状態に応じた角度を取得する
+				float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+				// 自キャラの角度を設定する
+				worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
+			}
+		} else {
+			// 非入力時は移動減衰をかける
+			velocity_.x *= (1.0f - kAttenuation);
+		}
+
+		if (Input::GetInstance()->PushKey(DIK_UP)) {
+			// ジャンプ初速
+			velocity_ += Vector3(0, kJumpAcceleration, 0);
+			velocity_.x += 0;
+			velocity_.y += kJumpAcceleration;
+			velocity_.z += 0;
+		}
+	} else {
+		// 落下速度
+		velocity_ += Vector3(0, -kGravityAcceleration, 0);
+		velocity_.x += 0;
+		velocity_.y += -kGravityAcceleration;
+		velocity_.z += 0;
+		// 落下速度制限
+		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
+
+		// 着地フラグ
+		landing = false;
+
+		// 地面との当たり判定
+		// 落下中？
+		if (velocity_.y < 0) {
+			// Y座標が地面以下になったら着地
+			if (worldTransform_.translation_.y <= 2.0f) {
+				landing = true;
+			}
+		}
+	}
+	// 移動
+	operator+=(worldTransform_.translation_, velocity_);
+	// 接地判定
+	if (onGround_) {
+		// ジャンプ開始
+		if (velocity_.y > 0.0f) {
+			// 空中状態に移行
+			onGround_ = false;
+		}
+	} else {
+		// 接地判定
+		if (landing) {
+			// めり込み排斥
+			worldTransform_.translation_.y = 1.0f;
+			// 摩擦で横方向速度が減衰する
+			velocity_.x *= (1.0f - kAcceleratio);
+			// 下方向速度のリセット
+			velocity_.y = 0.0f;
+			// 接地状態の移行
+			onGround_ = true;
+		}
+	}
+	// 行列計算
+	worldTransform_.UpdateMatrix();
+	// 行列を定数バッファに転送
+	worldTransform_.TransferMatrix();
 }
-}
 
-void Player::Draw(){
-    
+void Player::Draw() {
+	// 3Dモデルの描画
 	model_->Draw(worldTransform_, *viewProjection_, textureHandle_);
 }
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Model.h"
 #include "WorldTransform.h"
 
@@ -8,19 +7,17 @@ enum class LRDirection {
 	kLeft,
 };
 
-class Player{
+class Player {
 public:
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
 
-void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
-void Update();
-void Draw();
+	void Update();
+
+	void Draw();
+
 private:
-
-// ワールドトランスフォームの初期化
 	WorldTransform worldTransform_;
-	// モデル
 	Model* model_ = nullptr;
-	// テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
 
 	ViewProjection* viewProjection_ = nullptr;
@@ -28,17 +25,18 @@ private:
 	Vector3 velocity_ = {};
 
 	LRDirection lrDirection_ = LRDirection::kRight;
-	
+
 	float turnFirstRotationY_ = 0.0f;
 	float turnTimer_ = 0.7f;
-
 	bool onGround_ = true;
+
 	bool landing = false;
 
 	static inline const float kAcceleratio = 1.0f;
 	static inline const float kAttenuation = 0.1f;
-
 	static inline const float kLimitRunSpeed = 0.1f;
 	static inline const float kTimeTurn = 0.3f;
-
+	static inline const float kGravityAcceleration = 0.98f;
+	static inline const float kLimitFallSpeed = 0.5f;
+	static inline const float kJumpAcceleration = 3.0f;
 };
