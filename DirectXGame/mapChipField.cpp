67db0738d78065d1c2520730d49b18feb7a25868
maisawa <k023g1023@g.neec.ac.jp>
2024-07-09 10:29:55 +0900

    02_08_途中

--- DirectXGame/mapChipField.cpp
+++ DirectXGame/mapChipField.cpp
@@ -1,4 +1,3 @@
-
 #include "MapChipField.h"
 
 #include <cassert>
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include "Audio.h"
+#include "DebugCamera.h"
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
-#include "SafeDelete.h"
+#include "Player.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include <vector>
+#include "Skydome.h"
+#include "MapChipField.h"
+#include "CameraController.h"
 
 /// <summary>
 /// ゲームシーン
@@ -40,6 +45,8 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+	void GenerateBlocks();
+
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
@@ -48,4 +55,17 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	uint32_t textureHandle_ = 0;
+	Model* model_ = nullptr;
+	Model* modelBlock_ = nullptr;
+	Model* modelSkydome_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	Player* player_ = nullptr;
+	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
+	bool isDebugCameraActive_ = false;
+	DebugCamera* debugCamera_ = nullptr;
+	Skydome* skydome_ = nullptr;
+	MapChipField* mapChipField_ = nullptr;
+	CameraController* cameraController_ = nullptr;
 };
